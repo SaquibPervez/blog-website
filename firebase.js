9fc9f8ae859c404deeb6457ce6b6b91ee8e4db59
Saquib011 <saquibpervez011@gmail.com>
2025-01-11 20:41:02 +0500

    my blogs done

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@
  import { getAuth, createUserWithEmailAndPassword,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
  import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 
- import { doc, setDoc, getFirestore, getDoc,addDoc,collection,getDocs, updateDoc,deleteDoc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+ import { doc, setDoc, getFirestore, getDoc,addDoc,collection,getDocs, updateDoc,deleteDoc,query, where } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 
  // Your web app's Firebase configuration
  const firebaseConfig = {
@@ -24,5 +24,5 @@ const db = getFirestore(app);
  export{
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
-    auth,doc, setDoc, getDoc , db , onAuthStateChanged,addDoc,collection,getDocs,updateDoc,deleteDoc
+    auth,doc, setDoc, getDoc , db , onAuthStateChanged,addDoc,collection,getDocs,updateDoc,deleteDoc,query, where
  }
